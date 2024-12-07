d15860d87ee6a3aab5437d364754297ae02dd0ab
azzamasysyamil <azzamasysyamil@gmail.com>
2024-12-07 17:13:14 +0700

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const name = ref('Azzam');
+const status = ref('active');
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const newTask = ref('');
+
+const toggleStatus = () => {
+  if(status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = "inactive";
+  } else {
+    status.value = 'active'; 
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log('Error fetching tasks');
+  }
+});
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Task:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+  <br />
+  <button @click="toggleStatus">Change Status</button> 
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
